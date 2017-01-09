466d399912b2ca0b333227946b8c263a4a630b21
Dmitry Lisachenko <xdemonx07@gmail.com>
2017-01-09 16:39:12 +0300

    cppformat -> fmt

--- win-cpp-thirdparty/fmt-build.cmd
+++ win-cpp-thirdparty/fmt-build.cmd
@@ -1,7 +1,8 @@
 setlocal
-rem cppformat build
-set source_dir=cppformat
-set version=2.1
+rem fmt build
+set source_dir=fmt
+set libname=fmt
+set version=3.0.1
 set vcver=vc14
 
 call :build_platform %vcver%vars x86
@@ -16,8 +17,8 @@ setlocal
 call %1 %2
 
 rd build lib include /q/s > nul
-mkdir build lib include\cppformat
-copy %source_dir%\*.h include\cppformat
+mkdir build lib include\%libname%
+copy %source_dir%\*.h include\%libname%
 
 rem /Z7 - debug info
 rem /W4 - warning level 4
@@ -32,19 +33,19 @@ set RELEASE_DEFINES=/D NDEBUG /D WIN32 /D _WIN32_WINNT=0x501 /D _SCL_SECURE_NO_W
 set   DEBUG_DEFINES=/D _DEBUG /D WIN32 /D _WIN32_WINNT=0x501 /D _SCL_SECURE_NO_WARNINGS
 
 set FLAGS=%RELEASE_DEFINES% %RELEASE_CPPFLAGS% /MT
-call :build_lib cppformat-mt-s.lib
+call :build_lib lib%libname%-mt-s.lib
 
 set FLAGS=%DEBUG_DEFINES% %DEBUG_CPPFLAGS% /MTd
-call :build_lib cppformat-mt-sgd.lib
+call :build_lib lib%libname%-mt-sgd.lib
 
 set FLAGS=%RELEASE_DEFINES% %RELEASE_CPPFLAGS% /MD
-call :build_lib cppformat-mt.lib
+call :build_lib lib%libname%-mt.lib
 
 set FLAGS=%DEBUG_DEFINES% %DEBUG_CPPFLAGS% /MDd
-call :build_lib cppformat-mt-gd.lib
+call :build_lib lib%libname%-mt-gd.lib
 
 
-set arname=cppformat-%version%-%vcver%-%2.zip
+set arname=%libname%-%version%-%vcver%-%2.zip
 del %arname%
 7z a -tzip %arname% lib include
 
